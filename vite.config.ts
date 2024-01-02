3c7bfa072ec8a73e26763734f6fabc2c94b4b905
josechor <joserrapaz5@gmail.com>
2024-01-02 20:31:24 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  base: '/crewacap1',
+  base: 'https://josechor.github.io/crewcap1',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
